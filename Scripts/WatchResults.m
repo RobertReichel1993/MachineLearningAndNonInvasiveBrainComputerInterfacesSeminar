54a2709df32ede8f5d18ec4ab761f2fd22e8f199
Robert Reichel <robert.reichel@student.tugraz.at>
2021-04-20 17:29:20 +0200

    3 Class treatment for AC23 done, results visualized, just concatenation needed

--- Scripts/WatchResults.m
+++ Scripts/WatchResults.m
@@ -2,9 +2,16 @@ close all;
 clear all;
 clc;
 
+time_vec = {'Robert', 'Valeria', 'paper'};
+freq_vec = {'paper', 'csp_all', 'csp_bins'};
+%Loading results
 res_AC21 = load('..\Data\Results Patient AC21.mat');
 res_AC22 = load('..\Data\Results Patient AC22.mat');
 res_AC23 = load('..\Data\Results Patient AC23.mat');
+%Loading features
+feat_AC21 = load('..\Data\Features Patient AC21.mat');
+feat_AC22 = load('..\Data\Features Patient AC22.mat');
+feat_AC23 = load('..\Data\Features Patient AC23.mat');
 %Patient AC21
 AC21_freq_paper = mean(res_AC21.acc_AC21_freq_paper);
 AC21_freq_csp_all = mean(res_AC21.acc_AC21_freq_csp_all);
@@ -12,6 +19,12 @@ AC21_freq_csp_bins = mean(res_AC21.acc_AC21_freq_csp_bins);
 AC21_time_robert = mean(res_AC21.acc_AC21_time_robert);
 AC21_time_valeria = mean(res_AC21.acc_AC21_time_valeria);
 AC21_time_paper = mean(res_AC21.acc_AC21_time_paper);
+%Getting best performing methods
+[~, idx] = max([AC21_freq_paper, AC21_freq_csp_all, AC21_freq_csp_bins]);
+AC21_max_freq = freq_vec(idx);
+[~, idx] = max([AC21_time_robert, AC21_time_valeria, AC21_time_paper]);
+AC21_max_time = time_vec(idx);
+
 %Patient AC22
 AC22_freq_paper = mean(res_AC22.acc_AC22_freq_paper);
 AC22_freq_csp_all = mean(res_AC22.acc_AC22_freq_csp_all);
@@ -19,6 +32,12 @@ AC22_freq_csp_bins = mean(res_AC22.acc_AC22_freq_csp_bins);
 AC22_time_robert = mean(res_AC22.acc_AC22_time_robert);
 AC22_time_valeria = mean(res_AC22.acc_AC22_time_valeria);
 AC22_time_paper = mean(res_AC22.acc_AC22_time_paper);
+%Getting best performing methods
+[~, idx] = max([AC22_freq_paper, AC22_freq_csp_all, AC22_freq_csp_bins]);
+AC22_max_freq = freq_vec(idx);
+[~, idx] = max([AC22_time_robert, AC22_time_valeria, AC22_time_paper]);
+AC22_max_time = time_vec(idx);
+
 %Patient AC23
 AC23_freq_paper_1_2 = mean(res_AC23.acc_AC23_freq_paper_class_1_2);
 AC23_freq_paper_1_3 = mean(res_AC23.acc_AC23_freq_paper_class_1_3);
@@ -38,6 +57,20 @@ AC23_time_valeria_2_3 = mean(res_AC23.acc_AC23_time_valeria_class_2_3);
 AC23_time_paper_1_2 = mean(res_AC23.acc_AC23_time_paper_class_1_2);
 AC23_time_paper_1_3 = mean(res_AC23.acc_AC23_time_paper_class_1_3);
 AC23_time_paper_2_3 = mean(res_AC23.acc_AC23_time_paper_class_2_3);
+
+time_vec = {'Robert_12', 'Valeria_12', 'paper_12', 'Robert_13', ...
+    'Valeria_13', 'paper_13', 'Robert_23', 'Valeria_23', 'paper_23'};
+freq_vec = {'paper_12', 'csp_all_12', 'csp_bins_12', 'paper_13', ...
+    'csp_all_13', 'csp_bins_13', 'paper_23', 'csp_all_23', 'csp_bins_23'};
+%Getting best performing methods
+[~, idx] = max([AC23_freq_paper_1_2, AC23_freq_csp_all_1_2, AC23_freq_csp_bins_1_2, ...
+    AC23_freq_paper_1_3, AC23_freq_csp_all_1_3, AC23_freq_csp_bins_1_3, ...
+    AC23_freq_paper_2_3, AC23_freq_csp_all_2_3, AC23_freq_csp_bins_2_3]);
+AC23_max_freq = freq_vec(idx);
+[~, idx] = max([AC23_time_robert_1_2, AC23_time_valeria_1_2, AC23_time_paper_1_2, ...
+    AC23_time_robert_1_3, AC23_time_valeria_1_3, AC23_time_paper_1_3, ...
+    AC23_time_robert_2_3, AC23_time_valeria_2_3, AC23_time_paper_2_3]);
+AC23_max_time = time_vec(idx);
 
 %%
 %Patient AC21
